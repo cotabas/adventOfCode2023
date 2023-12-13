007ff0084a7e5dddb74956983d3f7c4e67a8219a
cotabas <cotabas@gmail.com>
2023-12-13 14:00:45 -0800

    part two day eleven

---
+++ day11/part2.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+func main() {
+  var result int
+  var meep [][]string
+  var colList, rowList []int
+  distMap := make(map[[2]string]int)
+  locMap := make(map[string][2]int) 
+  galMultiplier := 1000000 
+
+  file, _ := os.ReadFile(os.Args[1])
+  rows := strings.Split(string(file), "\n")
+  //add galaxy and expand rows
+  for x, row := range rows[:len(rows) - 1] {
+    if !strings.Contains(row, "#") {
+      rowList = append(rowList, x)
+    }
+    meep = append(meep, strings.Split(row, ""))
+  }
+
+  //expand columns
+  for _, row := range meep {
+    for y, pos := range row {
+      if pos == "#" {
+        colList = append(colList, y)
+      }
+    }
+  }
+
+  //number galaxies and add their location to map
+  var count int
+  for x, row := range meep {
+    for y, pos := range row {
+      if pos == "#" {
+        strCount := strconv.Itoa(count)
+        meep[x][y] = strCount 
+        locMap[strCount] = [2]int{x, y}
+        count++
+      }
+    }
+  }
+
+  //map shortest distances
+  for fGal, fxy := range locMap {
+    for sGal, sxy := range locMap {
+      var xD, yD int
+      if fGal == sGal { continue }
+      if fxy[0] > sxy[0] {
+        for i := sxy[0]; i < fxy[0]; i++ {
+          if slices.Contains(rowList, i) {
+            xD += galMultiplier
+          } else { xD++ }
+        }
+      } else {
+        for i := fxy[0]; i < sxy[0]; i++ {
+          if slices.Contains(rowList, i) {
+            xD += galMultiplier
+          } else { xD++ }
+        }
+      }
+      //for y coords
+      if fxy[1] > sxy[1] {
+        for i := sxy[1]; i < fxy[1]; i++ {
+          if !slices.Contains(colList, i) {
+            yD += galMultiplier
+          } else { yD++ }
+        }
+      } else {
+        for i := fxy[1]; i < sxy[1]; i++ {
+          if !slices.Contains(colList, i) {
+            yD += galMultiplier
+          } else { yD++ }
+        }
+      }
+      if distMap[[2]string{fGal, sGal}] == 0 && distMap[[2]string{sGal, fGal}] == 0 {
+        distMap[[2]string{fGal, sGal}] = xD + yD
+      }
+    }
+  }
+
+//  fmt.Println(distMap)
+//  for _, row := range meep {
+//    fmt.Println(row)
+//  }
+
+  for _, val := range distMap {
+    result += val
+  }
+
+  fmt.Println(result)
+}
