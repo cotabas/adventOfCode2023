a4a592742e5b1fda6da1fa8d14992ea1614f8c04
cotabas <cotabas@gmail.com>
2023-12-19 15:03:20 -0800

    ugh.. it's a lot harder when the input has so many more edge cases than the example

--- day18/part1.go
+++ day18/part1.go
@@ -1,13 +1,17 @@
 package main
 
 import (
-  "fmt"
-  "os"
-  "strings"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 func main() {
   var result int
+  meep := make(map[int][]int)
+  curLoc := []int{0, 0}
 
   file, _ := os.ReadFile(os.Args[1])
 
@@ -16,8 +20,65 @@ func main() {
 
   for _, line := range input {
     fmt.Println(line)
+    dir := strings.Split(line, " ")
+    var ud, rl int
+    switch dir[0] {
+    case "R":
+      rl = 1
+    case "L":
+      rl = -1
+    case "U":
+      ud = -1
+    case "D":
+      ud = 1
+    }
+    steps, _ := strconv.Atoi(dir[1])
+
+    for i := 1; i <= steps; i++ {
+      curLoc = []int{curLoc[0] + ud, curLoc[1] + rl}
+      meep[curLoc[0]] = append(meep[curLoc[0]], curLoc[1])
+    }
+
+
   }
 
+  // make a list of holes
+  // sort and count differences between highest and next highest
+  // map[xCoord][]int of y coords
 
+  for d, m := range meep { 
+    f := true
+    firstSect := true
+    first := 0
+    sort.IntSlice.Sort(m)
+    //
+    pr := result
+    //
+    result += len(m)
+    for dex, coord := range m {
+      if f {
+        if dex + 1 < len(m) && coord + 1 == m[dex + 1] { continue }
+        first = coord
+        f = false
+      } else {
+        if firstSect {
+          fmt.Println(coord, " - ", first, " = ", coord - first)
+          firstSect = false 
+          result += coord - first - 1
+          if dex + 1 < len(m) && coord + 1 != m[dex + 1] {
+            first = coord
+          } else { f = true }
+          continue
+        }
+        if dex + 1 < len(m) && coord + 1 != m[dex + 1] {
+          first = coord
+        } else { f = true }
+        firstSect = true
+      }
+    }
+    fmt.Println(d, m, "total:", result - pr)
+  }
+
+  fmt.Println(meep)
   fmt.Println(result)
 }
