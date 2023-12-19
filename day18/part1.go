fa3a7ad9e1b38fd5c3bb20ddedca66b08b8d3d0c
cotabas <cotabas@gmail.com>
2023-12-18 22:59:34 -0800

    day 18

---
+++ day18/part1.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+  "fmt"
+  "os"
+  "strings"
+)
+
+func main() {
+  var result int
+
+  file, _ := os.ReadFile(os.Args[1])
+
+  input := strings.Split(string(file), "\n")
+  input = input[:len(input) - 1]
+
+  for _, line := range input {
+    fmt.Println(line)
+  }
+
+
+  fmt.Println(result)
+}
